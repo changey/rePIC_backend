b06f61988b5c63eb1ff6b9236a17618ee2662dcd
Eric Chang <eric61213@gmail.com>
2013-07-03 09:39:29 -0700

    messages

--- messages.php
+++ messages.php
@@ -11,14 +11,25 @@ $url =$_GET['url'];
 
 include_once 'config.php';
 
-$receiver_array="[\"jill\"]";
+// $somearg = escapeshellarg('blah');
+// exec("php file2.php $somearg > /dev/null &");
+
+$receiver_array="[\"jill\",\"jace\"]";
+$time="2013-07-03 22:48:29";
 
 $receiver_decode = json_decode($receiver_array);
 
 echo count($receiver_decode);
+$length=count($receiver_decode);
+
+for($i=0;$i<$length;$i++){
+	echo $receiver_decode[$i];
+	$query = "INSERT INTO messages (sender, receiver, time, url, captions) VALUES('changey', '$receiver_decode[$i]', 'lala','baba','dada')";
+			mysql_query($query);
+}
 
 //$query = "INSERT INTO messages (sender, receiver, time, url, captions) VALUES('changey', '$receiver', 'lala','baba','dada')";
-			mysql_query($query);//make sure you're using the correct database//mysql_select_db('devices', $db) or die(mysql_error($db));
+//			mysql_query($query);//make sure you're using the correct database//mysql_select_db('devices', $db) or die(mysql_error($db));
 
 echo 'Data inserted successfully!';
 ?>
